4cb7117cb71bbee712c221376027542205216566
Victor <victor.rolland.leonetti@gmail.com>
2022-04-14 14:17:38 +0200

    functionnal tests App.test.tsx

--- Front/cypress/integration/testFront/front.spec.js
+++ Front/cypress/integration/testFront/front.spec.js
@@ -1,26 +1,26 @@
 describe("ProjetFinal", () => {
     it("Test add product", () => {
       cy.visit("http://localhost:3000");
-      cy.wait(20000);
+      cy.wait(7000);
       cy.contains("Figurine de Summer Smith").click();
       cy.contains("Ajouter au panier").click();
       cy.contains("Enregistré dans le panier")
       cy.contains("Retour").click();
-      cy.wait(20000);
+      cy.wait(7000);
       cy.contains("Aller sur panier").click();
-      cy.wait(20000);
+      cy.wait(7000);
       cy.contains("Figurine de Summer Smith");
     });
 
     it("Test delete product", () => {
         cy.visit("http://localhost:3000");
-        cy.wait(20000);
+        cy.wait(7000);
         cy.contains("Figurine de Summer Smith").click();
         cy.contains("Ajouter au panier").click();
         cy.contains("Retour").click();
-        cy.wait(20000);
+        cy.wait(7000);
         cy.contains("Aller sur panier").click();
-        cy.wait(20000);
+        cy.wait(7000);
         cy.contains("Supprimer du panier").click();
         cy.get('Figurine de Summer Smith').should('not.exist');
         cy.contains("Produit bien supprimé");
@@ -28,11 +28,11 @@ describe("ProjetFinal", () => {
 
     it("Test add product too much quantity", () => {
         cy.visit("http://localhost:3000");
-        cy.wait(20000);
+        cy.wait(7000);
         cy.contains("Figurine de Summer Smith").click();
         cy.get('input').type('00').type('{enter}')
         cy.contains("Ajouter au panier").click();
-        cy.wait(20000);
+        cy.wait(7000);
         cy.contains("Trop de quantité")
     });
 });
--- Front/src/App.test.tsx
+++ Front/src/App.test.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Cart from './components/Cart';
+import Product from './components/Product';
 
-test('renders learn react link', () => {
-  render(<App />);
-  //const linkElement = screen.getByText(/learn react/i);
-  //expect(linkElement).toBeInTheDocument();
+let container: any;
+let product = {
+  id: 1,
+  name: "string",
+  price: 12,
+  quantity: 20,
+  image: "string"
+};
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+test('renders cart link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Aller sur panier/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders return link', () => {
+  render(<Cart setRoute={function (cart): void { }} />);
+  const linkElement = screen.getByText(/Retour/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders product link', () => {
+  render(<Product setRoute={function (): void { }} data={product} />);
+  const linkElement = screen.getByText(/Ajouter au panier/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
