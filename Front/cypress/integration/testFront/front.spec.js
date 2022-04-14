62b5bc84f8fec42605872f81393b6cc95deab271
Victor <victor.rolland.leonetti@gmail.com>
2022-04-14 18:44:44 +0200

    cypress 30000ms

--- Front/cypress/integration/testFront/front.spec.js
+++ Front/cypress/integration/testFront/front.spec.js
@@ -1,51 +1,51 @@
 describe("ProjetFinal", () => {
     it("Test read product", () => {
         cy.visit("http://localhost:3000");
-        cy.wait(20000);
+        cy.wait(30000);
         cy.contains("Aller sur panier");
         cy.contains("Figurine de Summer Smith").click();
         cy.contains("Ajouter au panier")
         cy.contains("Retour").click();
-        cy.wait(20000);
+        cy.wait(30000);
         cy.contains("Aller sur panier");
       });
 
     it("Test add product", () => {
       cy.visit("http://localhost:3000");
-      cy.wait(20000);
+      cy.wait(30000);
       cy.contains("Figurine de Summer Smith").click();
       cy.contains("Ajouter au panier").click();
-      cy.wait(20000);
+      cy.wait(30000);
       cy.contains("Enregistré dans le panier")
       cy.contains("Retour").click();
-      cy.wait(20000);
+      cy.wait(30000);
       cy.contains("Aller sur panier").click();
-      cy.wait(20000);
+      cy.wait(30000);
       cy.contains("Figurine de Summer Smith");
     });
 
     it("Test delete product", () => {
         cy.visit("http://localhost:3000");
-        cy.wait(20000);
+        cy.wait(30000);
         cy.contains("Figurine de Summer Smith").click();
         cy.contains("Ajouter au panier").click();
         cy.contains("Retour").click();
-        cy.wait(20000);
+        cy.wait(30000);
         cy.contains("Aller sur panier").click();
-        cy.wait(20000);
+        cy.wait(30000);
         cy.contains("Supprimer du panier").click();
         cy.get('Figurine de Summer Smith').should('not.exist');
-        cy.wait(20000);
+        cy.wait(30000);
         cy.contains("Produit bien supprimé");
     });
 
     it("Test add product too much quantity", () => {
         cy.visit("http://localhost:3000");
-        cy.wait(20000);
+        cy.wait(30000);
         cy.contains("Figurine de Summer Smith").click();
         cy.get('input').type('00').type('{enter}')
         cy.contains("Ajouter au panier").click();
-        cy.wait(20000);
+        cy.wait(30000);
         cy.contains("Trop de quantité")
     });
 });
