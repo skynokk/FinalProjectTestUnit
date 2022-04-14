73f0bc4af01413e76d8d39093578af34b8973509
Victor <victor.rolland.leonetti@gmail.com>
2022-04-14 09:04:56 +0200

    Front sauf App.test.tsx

---
+++ Front/src/test/useHome.test.tsx
@@ -0,0 +1,51 @@
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { renderHook, act } from "@testing-library/react-hooks";
+import useHome from "../../hooks/useHome";
+
+const server = setupServer(
+    rest.get(
+        "http://localhost:8000/api/products",
+        (req, res, ctx) => {
+            return res(
+                ctx.json({
+                    products: [{
+                        id: 1,
+                        name: 'Rick Sanchez',
+                        price: 20,
+                        quantity: 20,
+                        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+                    },
+                    {
+                        id: 15,
+                        name: 'Alien Rick',
+                        price: 20,
+                        quantity: 20,
+                        image: 'https://rickandmortyapi.com/api/character/avatar/15.jpeg'
+                    },
+                    {
+                        id: 3,
+                        name: "Summer Smith",
+                        price: 67.6,
+                        quantity: 20,
+                        image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+                    }]
+                }))
+        }));
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("load product", async () => {
+    const { result } = renderHook(() => useHome());
+    const { loading, loadProducts } = result.current;
+    expect(loading).toEqual(true);
+    await act(async () => {
+        await loadProducts()
+    }
+    );
+    const { products } = result.current;
+    console.log(products)
+    expect(products).toEqual({"products": [{"id": 1, "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg", "name": "Rick Sanchez", "price": 20, "quantity": 20}, {"id": 15, "image": "https://rickandmortyapi.com/api/character/avatar/15.jpeg", "name": "Alien Rick", "price": 20, "quantity": 20}, {"id": 3, "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg", "name": "Summer Smith", "price": 67.6, "quantity": 20}]})
+});
---
+++ Front/src/test/useProduct.test.tsx
@@ -0,0 +1,58 @@
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { renderHook, act } from "@testing-library/react-hooks";
+import useProduct from "../../hooks/useProduct";
+import { Product } from "../../App";
+
+
+const server = setupServer(
+    rest.post(
+        "http://localhost:8000/api/cart/1",
+        (req, res, ctx) => {
+            return res(
+                ctx.json({
+                    products: [{
+                        id: 1,
+                        name: 'Rick Sanchez',
+                        price: 20,
+                        quantity: 20,
+                        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+                    },
+                    {
+                        id: 15,
+                        name: 'Alien Rick',
+                        price: 20,
+                        quantity: 20,
+                        image: 'https://rickandmortyapi.com/api/character/avatar/15.jpeg'
+                    },
+                    {
+                        id: 3,
+                        name: "Summer Smith",
+                        price: 67.6,
+                        quantity: 20,
+                        image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+                    }]
+                }))
+        }));
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('add products', async () => {
+    const addProducts: Product = {
+        id: 1,
+        name: 'Rick Sanchez',
+        price: '45.6',
+        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        quantity: 20,
+    }
+    const { result } = renderHook(() => useProduct(addProducts));
+    const { loading, addProduct } = result.current;
+    expect(loading).toEqual(false);
+    await act(async () => {
+        await addProduct();
+    });
+    const { message } = result.current;
+    console.log(message);
+});
